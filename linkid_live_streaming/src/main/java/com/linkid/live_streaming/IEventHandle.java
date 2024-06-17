3119c55477c80324417ff7ff31264903deef95c9
0x01001 <3379258+0x01001@users.noreply.github.com>
2024-06-17 14:42:19 +0700

    no message

--- linkid_live_streaming/src/main/java/com/linkid/live_streaming/IEventHandle.java
+++ linkid_live_streaming/src/main/java/com/linkid/live_streaming/IEventHandle.java
@@ -8,28 +8,28 @@ import org.json.JSONObject;
 // playerState: NO_PLAY(0), PLAY_REQUESTING(1), PLAYING(2);
 
 public interface IEventHandle {
-    // Login room result callback.
-    public void onLoginResult(int errorCode, boolean isHost, JSONObject extendedData);
-
-    public void onRoomStreamUpdate(String roomID, int updateType, String streamList, JSONObject extendedData);
-
-    // Callback for updates on the status of other users in the room.
-    // Users can only receive callbacks when the isUserStatusNotify property of ZegoRoomConfig is set to `true` when logging in to the room (loginRoom).
-    public void onRoomUserUpdate(String roomID, int updateType, String userList);
-
-    // Callback for updates on the current user's room connection status.
-    public void onRoomStateChanged(String roomID, int roomStateChangedReason, int i, JSONObject jsonObject);
-
-    // Status notification of audio and video stream publishing.
-    public void onPublisherStateUpdate(String streamID, int publisherState, int errorCode, JSONObject extendedData);
-
-    // Status notifications of audio and video stream playing.
-    // This callback is received when the status of audio and video stream
-    // playing of a user changes. If an exception occurs during stream playing
-    // due to a network interruption, the SDK automatically retries to play
-    // the streams.
-    public void onPlayerStateUpdate(String streamID, int playerState, int errorCode, JSONObject extendedData);
+//    // Login room result callback.
+//    public void onLoginResult(int errorCode, boolean isHost, JSONObject extendedData);
+//
+//    public void onRoomStreamUpdate(String roomID, int updateType, String streamList, JSONObject extendedData);
+//
+//    // Callback for updates on the status of other users in the room.
+//    // Users can only receive callbacks when the isUserStatusNotify property of ZegoRoomConfig is set to `true` when logging in to the room (loginRoom).
+//    public void onRoomUserUpdate(String roomID, int updateType, String userList);
+//
+//    // Callback for updates on the current user's room connection status.
+//    public void onRoomStateChanged(String roomID, int roomStateChangedReason, int i, JSONObject jsonObject);
+//
+//    // Status notification of audio and video stream publishing.
+//    public void onPublisherStateUpdate(String streamID, int publisherState, int errorCode, JSONObject extendedData);
+//
+//    // Status notifications of audio and video stream playing.
+//    // This callback is received when the status of audio and video stream
+//    // playing of a user changes. If an exception occurs during stream playing
+//    // due to a network interruption, the SDK automatically retries to play
+//    // the streams.
+//    public void onPlayerStateUpdate(String streamID, int playerState, int errorCode, JSONObject extendedData);
 
     // The callback to report the delivery result of the Broadcast Message
-    void onSendBroadcastMessageResult(int errorCode, String messageID);
+    void onSendHeartResult(int errorCode, String messageID);
 }
--- linkid_live_streaming/src/main/java/com/linkid/live_streaming/SdkManager.java
+++ linkid_live_streaming/src/main/java/com/linkid/live_streaming/SdkManager.java
@@ -38,6 +38,7 @@ public class SdkManager {
         Log.d("SdkManager", "init sdk...");
         this.data = data;
         this.eventHandle = eventHandle;
+        startListenEvent();
     }
 
     public void createEngine(Application application) {
@@ -126,7 +127,7 @@ public class SdkManager {
                 @Override
                 public void onIMSendBroadcastMessageResult(int errorCode, long messageID) {
                     //Handle the delivery result of the Broadcast Message.
-                    eventHandle.onSendBroadcastMessageResult(errorCode, Long.toString(messageID));
+                    eventHandle.onSendHeartResult(errorCode, Long.toString(messageID));
                 }
             });
         }else{
@@ -135,80 +136,81 @@ public class SdkManager {
                 @Override
                 public void onIMSendBarrageMessageResult(int errorCode, String messageID) {
                     // Handle the delivery result of the Barrage Message.
-                    eventHandle.onSendBroadcastMessageResult(errorCode, messageID);
+                    eventHandle.onSendHeartResult(errorCode, messageID);
                 }
             });
         }
     }
 
-    public void startListenEvent() {
+    private void startListenEvent() {
         ZegoExpressEngine.getEngine().setEventHandler(new IZegoEventHandler() {
-            @Override
-            // Callback for updates on the status of the streams in the room.
-            public void onRoomStreamUpdate(String roomID, ZegoUpdateType updateType, ArrayList<ZegoStream> streamList, JSONObject extendedData) {
-                try {
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    String jsonArray = objectMapper.writeValueAsString(streamList);
-                    eventHandle.onRoomStreamUpdate(roomID, updateType.value(), jsonArray, extendedData);
-                }
-                catch (JsonProcessingException e) {
-                    e.printStackTrace();
-                }
-            }
+//            @Override
+//            // Callback for updates on the status of the streams in the room.
+//            public void onRoomStreamUpdate(String roomID, ZegoUpdateType updateType, ArrayList<ZegoStream> streamList, JSONObject extendedData) {
+//                try {
+//                    ObjectMapper objectMapper = new ObjectMapper();
+//                    String jsonArray = objectMapper.writeValueAsString(streamList);
+//                    eventHandle.onRoomStreamUpdate(roomID, updateType.value(), jsonArray, extendedData);
+//                }
+//                catch (JsonProcessingException e) {
+//                    e.printStackTrace();
+//                }
+//            }
 
             @Override
             // Callback for updates on the status of other users in the room.
             // Users can only receive callbacks when the isUserStatusNotify property of ZegoRoomConfig is set to `true` when logging in to the room (loginRoom).
             public void onRoomUserUpdate(String roomID, ZegoUpdateType updateType, ArrayList<ZegoUser> userList) {
+                Log.d("onRoomUserUpdate: ", updateType.toString());
                 if (updateType == ZegoUpdateType.ADD) {
                     onlineUsers.addAll(userList);
                 } else if (updateType == ZegoUpdateType.DELETE) {
                     onlineUsers.removeAll(userList);
                 }
-                try {
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    String jsonArray = objectMapper.writeValueAsString(userList);
-                    eventHandle.onRoomUserUpdate(roomID, updateType.value(), jsonArray);
-                }
-                catch (JsonProcessingException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    ObjectMapper objectMapper = new ObjectMapper();
+//                    String jsonArray = objectMapper.writeValueAsString(userList);
+//                    eventHandle.onRoomUserUpdate(roomID, updateType.value(), jsonArray);
+//                }
+//                catch (JsonProcessingException e) {
+//                    e.printStackTrace();
+//                }
             }
 
-            @Override
-            // Callback for updates on the current user's room connection status.
-            public void onRoomStateChanged(String roomID, ZegoRoomStateChangedReason reason, int i, JSONObject jsonObject) {
-                if (data.isHost){
-                    if (reason == ZegoRoomStateChangedReason.LOGINED) {
-                        // Stream has started
-                        Log.d("LiveStream", "Stream started: " + roomID);
-                    } else if (reason == ZegoRoomStateChangedReason.LOGOUT) {
-                        // Stream has stopped
-                        Log.d("LiveStream", "Stream stopped: " + roomID);
-                    }
-                }
-                eventHandle.onRoomStateChanged(roomID, reason.value(), i, jsonObject);
-            }
-
-            // Status notification of audio and video stream publishing.
-            @Override
-            public void onPublisherStateUpdate(String streamID, ZegoPublisherState state, int errorCode, JSONObject extendedData) {
-                eventHandle.onPublisherStateUpdate(streamID, state.value(), errorCode, extendedData);
-            }
-
-            // Status notifications of audio and video stream playing.
-            // This callback is received when the status of audio and video stream
-            // playing of a user changes. If an exception occurs during stream playing
-            // due to a network interruption, the SDK automatically retries to play
-            // the streams.
-            @Override
-            public void onPlayerStateUpdate(String streamID, ZegoPlayerState state, int errorCode, JSONObject extendedData) {
-                eventHandle.onPlayerStateUpdate(streamID, state.value(), errorCode, extendedData);
-            }
+//            @Override
+//            // Callback for updates on the current user's room connection status.
+//            public void onRoomStateChanged(String roomID, ZegoRoomStateChangedReason reason, int i, JSONObject jsonObject) {
+//                if (data.isHost){
+//                    if (reason == ZegoRoomStateChangedReason.LOGINED) {
+//                        // Stream has started
+//                        Log.d("LiveStream", "Stream started: " + roomID);
+//                    } else if (reason == ZegoRoomStateChangedReason.LOGOUT) {
+//                        // Stream has stopped
+//                        Log.d("LiveStream", "Stream stopped: " + roomID);
+//                    }
+//                }
+//                eventHandle.onRoomStateChanged(roomID, reason.value(), i, jsonObject);
+//            }
+//
+//            // Status notification of audio and video stream publishing.
+//            @Override
+//            public void onPublisherStateUpdate(String streamID, ZegoPublisherState state, int errorCode, JSONObject extendedData) {
+//                eventHandle.onPublisherStateUpdate(streamID, state.value(), errorCode, extendedData);
+//            }
+//
+//            // Status notifications of audio and video stream playing.
+//            // This callback is received when the status of audio and video stream
+//            // playing of a user changes. If an exception occurs during stream playing
+//            // due to a network interruption, the SDK automatically retries to play
+//            // the streams.
+//            @Override
+//            public void onPlayerStateUpdate(String streamID, ZegoPlayerState state, int errorCode, JSONObject extendedData) {
+//                eventHandle.onPlayerStateUpdate(streamID, state.value(), errorCode, extendedData);
+//            }
         });
     }
 
-    public void stopListenEvent() {
+    public void exit() {
         this.eventHandle = null;
         ZegoExpressEngine.getEngine().setEventHandler(null);
     }
